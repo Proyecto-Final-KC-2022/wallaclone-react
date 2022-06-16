b2889a75e4a7018f7a45cec033e53c65267f6d7a
Rodolfo Rodriguez <rodolfo2smr@gmail.com>
2022-06-16 23:25:29 +0200

    chore: añadido ejemplo de conexion con nuestro backend en nodejs, REVISAR LOS TO DOs

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,22 +1,66 @@
 import Header from "../../components/header/Header";
-import Footer from "../../components/footer/Footer"
+import Footer from "../../components/footer/Footer";
 
 import Login from "../../components/login/Login";
+import { useEffect, useState } from "react";
 
 const style = {
   homepageWrapper: "flex w-full h-screen bg-gray-500",
+};
+
+async function getAdvertisements() {
+  let httpResponse;
+  let advertisements;
+  try {
+    //TODO: EL FQDN "http://localhost:3000" DEBERÍA SER UNA VARIABLE DE ENTORNO PARA QUE SE PEUEDA CONFIGURAR DESDE EL SERVIDOR
+    //UISAR DOTENV
+    httpResponse = await fetch("http://localhost:3000/advertisements");
+  } catch (error) {
+    throw new Error("ERROR IN HTTP REQUEST");
+  }
+
+  if (!httpResponse.ok) {
+    throw new Error(
+      `Anuncios no encontrados. Status => ${httpResponse.status}`
+    );
+  }
+
+  try {
+    advertisements = await httpResponse.json();
+  } catch (error) {
+    throw new Error("ERROR FORMATTING TO JSON");
+  }
+
+  return advertisements;
 }
 
 const HomePage = () => {
+  const [ads, setAds] = useState([]);
+
+  useEffect(() => {
+    (async function fetchTweets() {
+      try {
+        const adstList = await getAdvertisements();
+        setAds(adstList);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
   return (
     <>
       <Header />
-        <div className={style.homepageWrapper}>
-          HomePage
-        </div> 
-      <Footer />
-    </>  
-  )
-}
+      <div className={style.homepageWrapper}>HomePage</div>
 
-export default HomePage
+      {/* //TODO: BORRAR EJEMPLO PETICION*/}
+      <ul>
+        {ads.map((ad: any) => {
+          return <li>{ad.name}</li>;
+        })}
+      </ul>
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
