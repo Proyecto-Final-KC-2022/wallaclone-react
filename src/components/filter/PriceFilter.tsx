6bd7f191e9cb66578f69afa04f8bd644b976fe91
EmerBV <emersmc@hotmail.com>
2022-07-09 19:25:33 +0200

    tags & price filter responsive

--- src/components/filter/PriceFilter.tsx
+++ src/components/filter/PriceFilter.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
+import PriceFilterResponsive from "./PriceFilterResponsive";
 
 function valuetext(value: number) {
   return `${value}€`;
@@ -13,15 +14,19 @@ const PriceFilter = () => {
     setValue(newValue as number[]);
   };
   return (
-    <div className="block fixed top-[130px] left-[234px] mx-[1rem] bg-white rounded-[10px] shadow-md overflow-hidden min-w-[380px]">
-      <div className="flex h-[70px] px-[1.5rem] items-center">
-        <p className="font-bold m-0 block">¿Cuánto quieres pagar?</p>
-      </div>
-      <div className="overflow-auto px-[1.5rem] block">
-        <div className="block">
-          <form className="overflow-hidden block">
-            <form className="flex items-center justify-center">
-                <Box className="py-[40px] px-[1.5rem] inline-block relative w-full" sx={{ width: 332, color: "success.main" }}>
+    <>
+      <div className="hidden lg:block md:block fixed top-[130px] left-[234px] mx-[1rem] bg-white rounded-[10px] shadow-md overflow-hidden min-w-[380px]">
+        <div className="flex h-[70px] px-[1.5rem] items-center">
+          <p className="font-bold m-0 block">¿Cuánto quieres pagar?</p>
+        </div>
+        <div className="overflow-auto px-[1.5rem] block">
+          <div className="block">
+            <form className="overflow-hidden block">
+              <form className="flex items-center justify-center">
+                <Box
+                  className="py-[40px] px-[1.5rem] inline-block relative w-full"
+                  sx={{ width: 332, color: "success.main" }}
+                >
                   <Slider
                     getAriaLabel={() => "Rango de precios"}
                     value={value}
@@ -30,34 +35,39 @@ const PriceFilter = () => {
                     getAriaValueText={valuetext}
                   />
                 </Box>
-            </form>
-            <div className="mt-[1rem] flex">
+              </form>
+              <div className="mt-[1rem] flex items-center justify-center">
                 <div className="mr-[0.5rem] w-full flex flex-col">
-                    <label className="inline-block mb-[0.5rem]">Desde</label>
-                    <input 
-                        className="rounded-[6px] text-[1rem] border border-[#CFD8DC] pl-[0.5rem] h-[50px] max-w-[158px] overflow-visible" 
-                        placeholder="{value}"
-                    />
+                  <label className="inline-block mb-[0.5rem]">Desde</label>
+                  <input
+                    className="rounded-[6px] text-[1rem] border border-[#CFD8DC] pl-[0.5rem] h-[50px] max-w-[158px] overflow-visible"
+                    placeholder="{value}"
+                  />
                 </div>
 
-                <div className="mr-[0.5rem] flex flex-col">
-                    <label className="inline-block mb-[0.5rem]">Hasta</label>
-                    <input 
-                        className="rounded-[6px] text-[1rem] border border-[#CFD8DC] pl-[0.5rem] h-[50px] max-w-[158px] overflow-visible" 
-                        placeholder="{value}"
-                    />
+                <div className=" flex flex-col">
+                  <label className="inline-block mb-[0.5rem]">Hasta</label>
+                  <input
+                    className="rounded-[6px] text-[1rem] border border-[#CFD8DC] pl-[0.5rem] h-[50px] max-w-[158px] overflow-visible"
+                    placeholder="{value}"
+                  />
                 </div>
-            </div>
-          </form>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="h-[88px] px-[1.5rem] items-center justify-end flex">
+          <button className="py-[8px] px-[20px] cursor-pointer">
+            Cancelar
+          </button>
+          <button className="py-[8px] cursor-pointer text-white px-[20px] rounded-[25px] bg-[#13c1ac] hover:bg-[#0f9989] border border-[#13c1ac]">
+            Aplicar
+          </button>
         </div>
       </div>
-      <div className="h-[88px] px-[1.5rem] items-center justify-end flex">
-        <button className="py-[8px] px-[20px] cursor-pointer">
-            Cancelar
-        </button>
-        <button className="py-[8px] cursor-pointer text-white px-[20px] rounded-[25px] bg-[#13c1ac] hover:bg-[#0f9989] border border-[#13c1ac]">Aplicar</button>
-      </div>
-    </div>
+
+      <PriceFilterResponsive />
+    </>
   );
 };
 
--- src/components/filter/TagsFilter.tsx
+++ src/components/filter/TagsFilter.tsx
@@ -1,55 +1,71 @@
+import TagsFilterResponsive from "./TagsFilterResponsive";
+
 const TagsFilter = () => {
   return (
-    <div className="block fixed top-[130px] mx-[1rem] bg-white rounded-[10px] shadow-md overflow-hidden min-w-[380px]">
-      <div className="flex h-[70px] px-[1.5rem] items-center">
-        <p className="font-bold m-0 block">Categoría</p>
-      </div>
-      <div className="overflow-auto px-[1.5rem] block">
-        <div className="block">
-          <form className="w-full block">
-            <div className="max-w-[720px] px-[15px] mx-auto block">
-              <div className="flex flex-wrap">
+    <>
+      <div className="hidden lg:block md:block fixed top-[130px] mx-[1rem] bg-white rounded-[10px] shadow-md overflow-hidden min-w-[380px]">
+        <div className="flex h-[70px] px-[1.5rem] items-center">
+          <p className="font-bold m-0 block">Categoría</p>
+        </div>
+        <div className="max-h-[calc(100% - 158px)] overflow-auto px-[1.5rem] block">
+          <div className="block">
+            <form className="w-full block">
+              <div className="max-w-[720px] px-[15px] mx-auto block">
+                <div className="flex flex-wrap">
+                  <div className="flex max-w-[25%]">
+                    <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
+                      <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">
+                        Todas las categorías
+                      </span>
+                    </div>
+                  </div>
 
-                <div className="flex max-w-[25%]">
-                  <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
-                    <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">Todas las categorías</span>
+                  <div className="flex max-w-[25%]">
+                    <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
+                      <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">
+                        Coches
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="flex max-w-[25%]">
+                    <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
+                      <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">
+                        Motos
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="flex max-w-[25%]">
+                    <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
+                      <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">
+                        Motor y Accesorios
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="flex max-w-[25%]">
+                    <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
+                      <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">
+                        Moda y Accesorios
+                      </span>
+                    </div>
                   </div>
                 </div>
-
-                <div className="flex max-w-[25%]">
-                  <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
-                    <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">Coches</span>
-                  </div>
-                </div>
-
-                <div className="flex max-w-[25%]">
-                  <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
-                    <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">Motos</span>
-                  </div>
-                </div>
-
-                <div className="flex max-w-[25%]">
-                  <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
-                    <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">Motor y Accesorios</span>
-                  </div>
-                </div>
-
-                <div className="flex max-w-[25%]">
-                  <div className="min-h-[96px] cursor-pointer p-[0.5rem] items-center justify-center flex-col flex">
-                    <span className="mt-[5px] text-[.875rem] text-center cursor-pointer hover:text-[#13c1ac]">Moda y Accesorios</span>
-                  </div>
-                </div>
-
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
+        </div>
+        <div className="h-[88px] px-[1.5rem] items center justify-end flex">
+          <button className="bg-transparent py-[8px] cursor-pointer">
+            Cancelar
+          </button>
         </div>
       </div>
-      <div className="h-[88px] px-[1.5rem] items center justify-end flex">
-        <button className="bg-transparent py-[8px] cursor-pointer">Cancelar</button>
-      </div>
-    </div>
-  )
-}
 
-export default TagsFilter
+      <TagsFilterResponsive />
+    </>
+  );
+};
+
+export default TagsFilter;
