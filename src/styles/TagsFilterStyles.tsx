a9f5e0137db1bc69ced076f1f2c0eb64e1dd92f1
EmerBV <emersmc@hotmail.com>
2022-07-27 01:47:52 +0200

    responsive filters done

--- src/styles/TagsFilterStyles.tsx
+++ src/styles/TagsFilterStyles.tsx
@@ -2,7 +2,7 @@ const TagsFilterStyles = {
   /* tagsFilterWrapper:
     "hidden lg:block md:block fixed top-[130px] mx-[1rem] bg-white rounded-[10px] shadow-md overflow-hidden min-w-[380px]", */
   tagsFilterWrapper:
-    "block fixed top-[130px] mx-[1rem] bg-white rounded-[10px] shadow-md overflow-hidden min-w-[380px]",
+    "block fixed bg-white rounded-[10px] shadow-md overflow-hidden lg:top-[130px] lg:mx-[1rem] lg:min-w-[380px] lg:w-[648px] lg:h-[380px] md:top-[130px] md:mx-[1rem] md:min-w-[380px] md:w-[648px] md:h-[380px] left-0 right-0 top-[66px] w-full h-full min-w-auto",
   tagsFilterHeader: "flex h-[70px] px-[1.5rem] items-center",
   tagsHeaderText: "font-bold m-0 block",
   tagsFormWrapper: "overflow-auto px-[1.5rem] block",
@@ -17,7 +17,7 @@ const TagsFilterStyles = {
   tagsText: "mt-[5px] text-[.875rem] text-center cursor-pointer",
   warningContainer:
     "flex items-center rounded-[10px] border p-[0.5rem] text-red-400",
-  buttonsWrapper: "h-full px-[1.5rem] items center justify-end flex my-4",
+  buttonsWrapper: "px-[1.5rem] items center justify-end flex lg:my-4 md:my-4 h-[48px]",
   cancelButton: "bg-transparent py-[8px] cursor-pointer mr-4",
   applyButton:
     "py-[8px] cursor-pointer text-white px-[20px] rounded-[25px] bg-[#13c1ac] hover:bg-[#0f9989] border border-[#13c1ac]",
